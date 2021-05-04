8b4d8cf9ba1cca4641b004c6f58ee31f64f0a78c
matt-hu <matt.hu1@outlook.com>
2021-05-04 20:01:43 +0100

    Clean up css for topbar.vue

--- src/App.vue
+++ src/App.vue
@@ -21,16 +21,4 @@ export default defineComponent({
     color: #2c3e50;
 }
 
-#nav {
-    padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
-}
 </style>
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,26 +1,14 @@
 <template>
     <div class="topnav">
-        <!-- <div class="row"> -->
-            <!-- <div class="nav-btn-div"> -->
-            <!-- <div class="column-2"> -->
-                <button class="nav-btn" id="nav-open">☰</button>
-            <!-- </div> -->
-            <!-- <div class="column-6 align-center"> -->
-            <!-- <div class="align-center"> -->
-                <div  class="throttle-heading"> 
-                    <!-- <a href="http://www.dcc-ex.com" target="_blank" rel="noopener noreferrer"><div class="wt-logo"></div></a> -->
-                    <a href="http://www.dcc-ex.com" target="_blank" rel="noopener noreferrer"><img class="wt-logo" src="@/assets/WebThrottle.png" alt="DCC-EX logo"></a>
-                    <!--img src="images/webthrottle.png" /> -->
-                    <!-- <p>version 1.2.0</p> -->
-                </div>
-            <!-- </div> -->
-            <!-- <div class="column-2"> -->
-            <div class="btn-cluster">
-                <button class="btn-expand" id="fs-toggle" state="ws" title="fullscreen">&#10530;</button>
-                <button class="btn-info" id="info-tooltip" state="ws" title="information"><span class="icon-info"></span></button>
-                <button class="btn-info" id="help-button" state="ws" title="help" onclick=" window.open('https://dcc-ex.com/throttles/ex-webthrottle.html', '_blank')"><span class="icon-question"></span></button>
-            </div>
-        <!-- </div>  -->
+        <button class="nav-btn" id="nav-open">☰</button>
+        <div  class="throttle-heading"> 
+            <a href="http://www.dcc-ex.com" target="_blank" rel="noopener noreferrer"><img class="wt-logo" src="@/assets/WebThrottle.png" alt="DCC-EX logo"></a>
+        </div>
+        <div class="btn-cluster">
+            <button class="btn-expand" id="fs-toggle" state="ws" title="fullscreen">&#10530;</button>
+            <button class="btn-info" id="info-tooltip" state="ws" title="information"><span class="icon-info"></span></button>
+            <button class="btn-info" id="help-button" state="ws" title="help" onclick=" window.open('https://dcc-ex.com/throttles/ex-webthrottle.html', '_blank')"><span class="icon-question"></span></button>
+        </div>
     </div>
 </template>
 
@@ -35,56 +23,15 @@ export default {
     display: flex;
     justify-content: space-between;
 }
-/* .nav-btn {
-    text-align: left;
-    width: 10%;
-    float: left;
-    padding: 5px, 15px;
-}
-.throttle-heading{
-    float: left;
-    /* width: 60%; */
-    /* padding: 5px 15px; */
-    /* text-align: center; */
-    /* padding-top:5px; */
-    /* font-size: 14px; */
-    /* display: inline-block; */
-/* } */
-/* .topnav { */
-    /* width: 100%; */
-/* } */
-/* .btn-cluster { */
-    /* padding: 5px 15px; */
-/*}
-
-/* .topnav .row div, .add-loco-form .row label, .add-loco-form .row input{
-    box-sizing: border-box;
-} */
-/* .row:after {
-    content: "";
-    display: block;
-    clear: both;
-} */
-/* .topnav{
-    position: fixed;
-    z-index: 100;
-    width: 100%;
-} */
- .nav-btn {
+.nav-btn {
     font-size: 30px;
     cursor: pointer;
     color: #666666;
     padding: 10px 15px;
+    padding-right: 108px;
     border: none;
     background: none;
     line-height: 38px;
-}
-.nav-btn.in{
-    color:#cccccc;
-    font-size: 40px;
-    position: absolute;
-    right: 0;
-    line-height: 30px;
 }
 .nav-btn:hover {
   color: #000000;
@@ -110,21 +57,14 @@ export default {
 }
  .throttle-heading{
     text-align: center;
-    padding-top:5px;
+    padding-top:15px;
     font-size: 14px;
     display: inline-block;
 } 
 .throttle-heading .wt-logo{
     height: 48px;
     width: 142px;
-    /* background: url(@/assets/WebThrottle.png); */
-    display: inline-block;
-    background-size: 100%;
     box-shadow: 0 0 3px #989898;
     border-radius: 6px;
 }
-.throttle-heading p{
-    font-size: 12px;
-    margin: 5px;
-}  
 </style>
