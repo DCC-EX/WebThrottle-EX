55ecfe0b55a2f375c428dfa82946f80fec0e7560
David Young <dyoung@thoughtbot.com>
2021-12-02 08:45:55 +0000

    Emulator code slightly refactored.
    Brings variables inside of function to avoid leaking.
    Refactored `if` statement to act as a [guard clause](https://medium.com/@scadge/if-statements-design-guard-clauses-might-be-all-you-need-67219a1a981a).
    Renamed variables to encourage immutability

--- js/emulator.js
+++ js/emulator.js
@@ -8,66 +8,66 @@
     file manages the correct response that a Command Station would provide.
 */
 
-let lastMessage;
-var turnouts = [];
 function emulator(packet) {
-    if (packet == "<credits>") {
-        console.log("Credits")
-        return credits()
-    } else {
-    packetKey = packet[1];
-    if (packetKey == ' ') {
-        packetKey = packet[2];
-    }
-    packet = packet.split(" ");
-    switch (packetKey) {
-        // Cab control
-        case ("t"):
-            lastMessage = 'T 1 '+ packet[3] + ' ' + packet[4].substring(0, packet[4].length-2)
-            return lastMessage;
-            
-        // Track power off
-        case ('0') :
-            lastMessage = 'p0';
-            return lastMessage;
+  let lastMessage;
+  var turnouts = [];
 
-        // Track power on
-        case ('1'):
-            lastMessage = 'p1';
-            return lastMessage;
+  if (packet == "<credits>") {
+    console.log("Credits")
+    return credits()
+  }
 
-        // New cab functions
-        case ('F'):
-            return NaN;
-        
-        // Legacy cab functions
-        case ('f'):
-            return NaN;
-        
-        // Turnouts
-        case ('T'): //Not fully finished
-            if (packet.length == 4) {
-                turnouts.push({id: packet[1], address: packet[2], subaddress: packet[3], throw: 0})
-                return '<O>';
-            } else if (packet.length == 2) {
-                var i;
-                for (i=0; i<turnouts.length; i++ ) {
-                    if (turnouts[i]['id'] == packet[1].substring(0, packet[1].length-1)) {
-                        turnouts.splice(i, 1);
-                        return 'O';
-                    }
-                }
-                return 'X';
-            } else if (packet.length == 1) {
-                returnList = [];
-                if (turnouts.length > 0) {
-                    for (i=0; i<turnouts.length; i++){
-                        returnList.push('H '+turnouts[i]['id']+' '+turnouts[i]['address']+' '+turnouts[i]['subaddress']+' '+turnouts[i]['throw']);
-                    }
-                return returnList;
-                }
-                return 'X';
-            }
+  const cleanedPacket = [...packet].filter(char => !["<", ">"].includes(char))
+  let packetKey = cleanedPacket.find(char => char !== " ");
+
+  const splitPacket = packet.split(" ");
+  switch (packetKey) {
+    // Cab control
+    case ("t"):
+      lastMessage = 'T 1 ' + splitPacket[3] + ' ' + splitPacket[4].substring(0, splitPacket[4].length - 2)
+      return lastMessage;
+
+    // Track power off
+    case ('0') :
+      lastMessage = 'p0';
+      return lastMessage;
+
+    // Track power on
+    case ('1'):
+      lastMessage = 'p1';
+      return lastMessage;
+
+    // New cab functions
+    case ('F'):
+      return NaN;
+
+    // Legacy cab functions
+    case ('f'):
+      return NaN;
+
+    // Turnouts
+    case ('T'): //Not fully finished
+      if (splitPacket.length == 4) {
+        turnouts.push({id: splitPacket[1], address: splitPacket[2], subaddress: splitPacket[3], throw: 0})
+        return '<O>';
+      } else if (splitPacket.length == 2) {
+        var i;
+        for (i = 0; i < turnouts.length; i++) {
+          if (turnouts[i]['id'] == splitPacket[1].substring(0, splitPacket[1].length - 1)) {
+            turnouts.splice(i, 1);
+            return 'O';
+          }
         }
-    }
+        return 'X';
+      } else if (splitPacket.length == 1) {
+        returnList = [];
+        if (turnouts.length > 0) {
+          for (i = 0; i < turnouts.length; i++) {
+            returnList.push('H ' + turnouts[i]['id'] + ' ' + turnouts[i]['address'] + ' ' + turnouts[i]['subaddress'] + ' ' + turnouts[i]['throw']);
+          }
+          return returnList;
+        }
+        return 'X';
+      }
+  }
 }
