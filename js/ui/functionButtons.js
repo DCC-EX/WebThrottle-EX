b9b94ba5fd72a8ad432a9524a6cc9eb75c5d4b29
David Young <dyoung@thoughtbot.com>
2021-12-16 14:06:42 +0000

    Removed jQuery from event handlers

--- js/ui/functionButtons.js
+++ js/ui/functionButtons.js
@@ -1,7 +1,7 @@
 import {cabCommand} from 'https://cdn.skypack.dev/@cloudthrottle/dcc-ex--commands';
 
 function sendCabCommand(fn, value) {
-  setFunCurrentVal(fn,value);
+  setFunCurrentVal(fn, value);
   const cab = getCV()
   const func = fn.replace("f", "")
   const command = cabCommand({cab, func, value})
@@ -10,50 +10,67 @@ function sendCabCommand(fn, value) {
 
 // This function will generate commands for each type of function
 function generateFnCommand(clickedBtn) {
-  const func = clickedBtn.attr('name'); // Gives function name (F1, F2, .... F28)
-  const btnPressed = clickedBtn.attr("aria-pressed");
-  const opr = btnPressed==="true" ? 1: 0
+  const func = clickedBtn.getAttribute('name'); // Gives function name (F1, F2, .... F28)
+  const btnPressed = clickedBtn.getAttribute("aria-pressed");
+  const opr = btnPressed === "true" ? 1 : 0
 
   sendCabCommand(func, opr)
 }
 
+let timers = {}
+
+function functionButtonPressed(buttonElement) {
+  console.log("Pressed", buttonElement);
+  const {dataset: {type: buttonType}} = buttonElement
+
+  if (buttonType === "press") {
+    timers[buttonElement.id] = setInterval(function () {
+      // MOMENTARY HOLD ON
+      buttonElement.setAttribute("aria-pressed", "true");
+      generateFnCommand(buttonElement);
+      console.debug("PRESSED HOLD ==> " + buttonElement.getAttribute("name"));
+    }, 100);
+  }
+}
+
+function functionButtonReleased(buttonElement) {
+  clearInterval(timers[buttonElement.id]);
+  console.log("Released", buttonElement);
+  const {dataset: {type: buttonType}} = buttonElement
+  const btnState = buttonElement.getAttribute("aria-pressed");
+
+  if (buttonType === "press") {
+    buttonElement.setAttribute("aria-pressed", "false");
+    generateFnCommand(buttonElement);
+    console.debug("RELEASED HOLD  ==> " + buttonElement.getAttribute("name"));
+  } else {
+    if (btnState === "false") {
+      // TOGGLE ON
+      buttonElement.setAttribute("aria-pressed", "true");
+      generateFnCommand(buttonElement);
+      console.debug("TOGGLE ON ==> " + buttonElement.getAttribute("name"));
+    } else {
+      // TOGGLE OFF
+      buttonElement.setAttribute("aria-pressed", "false");
+      generateFnCommand(buttonElement);
+      console.debug("TOGGLE OFF ==> " + buttonElement.getAttribute("name"));
+    }
+  }
+}
+
 // Functions buttons
 // Send Instructions to generate command depends the type of Button (press/toggle)
-var timer = 0;
-$(document)
-  .on("mousedown", ".fn-btn", function () {
-    const clickedBtn = $(this);
-    const btnType = clickedBtn.data("type");
-    if (btnType === "press") {
-      timer = setInterval(function () {
-        // MOMENTARY HOLD ON
-        clickedBtn.attr("aria-pressed", "true");
-        generateFnCommand(clickedBtn);
-        console.debug("PRESSED HOLD ==> " + clickedBtn.attr("name"));
-      }, 100);
-    }
-  })
-  .on("mouseup mouserelease", ".fn-btn", function () {
-    clearInterval(timer);
-    const clickedBtn = $(this);
-    const btnType = clickedBtn.data("type");
-    const btnState = clickedBtn.attr("aria-pressed");
-    if (btnType === "press") {
-      // MOMENTARY HOLD OFF
-      clickedBtn.attr("aria-pressed", "false");
-      generateFnCommand(clickedBtn);
-      console.debug("RELEASED HOLD  ==> " + clickedBtn.attr("name"));
-    } else {
-      if (btnState === "false") {
-        // TOGGLE ON
-        clickedBtn.attr("aria-pressed", "true");
-        generateFnCommand(clickedBtn);
-        console.debug("TOGGLE ON ==> " + clickedBtn.attr("name"));
-      } else {
-        // TOGGLE OFF
-        clickedBtn.attr("aria-pressed", "false");
-        generateFnCommand(clickedBtn);
-        console.debug("TOGGLE OFF ==> " + clickedBtn.attr("name"));
-      }
-    }
-  });
+const fnWrapperElement = document.getElementById("fn-wrapper")
+
+fnWrapperElement.addEventListener("mousedown", (event) => {
+  const {target} = event
+  if ([...target.classList].includes("fn-btn")) {
+    functionButtonPressed(target)
+  }
+})
+fnWrapperElement.addEventListener("mouseup", (event) => {
+  const {target} = event
+  if ([...target.classList].includes("fn-btn")) {
+    functionButtonReleased(target)
+  }
+})
