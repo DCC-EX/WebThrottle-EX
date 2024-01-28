5b99b9777bd1704d47645d9a0d8827f57fc61049
Peter Akers <akersp62@gmail.com>
2024-01-28 10:40:10 +1000

    cache again

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 var cacheName = 'WebThrottle-EX';
 var filesToCache = [
-  '/WebThrottle-EX/',
-  'index.html',
   'images/favicon.ico',
   'images/carbon_fibre.png',
   'images/cover.png',
@@ -17,34 +15,27 @@ var filesToCache = [
   'css/icons.css',
   'css/jquery-ui.css',
   'css/jquery.rotaryswitch.css',
-  'css/layout.css',
   'css/pwa.css',
   'css/roundslider.min.css',
   'css/settings.css',
-  'css/throttle.css',
   'css/fonts/icomoon.eot',
   'css/fonts/icomoon.svg',
   'css/fonts/icomoon.ttf',
   'css/fonts/icomoon.woff',
   'css/themes/dark.css',
   'css/themes/metallic.css',
-  'js/addloco.js',
-  'js/commandController.js',
-  'js/emulator.js',
-  'js/exwebthrottle.js',
-  'js/fnMaster.js',
   'js/jquery-3.2.1.min.js',
   'js/jquery-ui.min.js',
   'js/jquery.rotaryswitch.js',
   'js/pwa.js',
-  'js/roundslider.min.js',
-  'js/storageController.js'
+  'js/roundslider.min.js'
 ];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
+      cache.delete('index.html');
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
