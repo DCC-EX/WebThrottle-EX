7efd17fee36f78a6325a936739822e7946ecf316
Peter Akers <akersp62@gmail.com>
2024-02-29 09:09:31 +1000

    Update commandController.js

--- js/commandController.js
+++ js/commandController.js
@@ -503,17 +503,17 @@ function browserType() {
     
     // Detect Chrome
     if (/Chrome/.test(userAgent) && !/Chromium/.test(userAgent)) {
-        browser = "Google Chrome (Chromium)";
+        browser = "Google Chrome";
         browserOk = true;
     }
     // Detect Chromium-based Edge
     else if (/Edg/.test(userAgent)) {
-        browser = "Microsoft Edge (Chromium)";
+        browser = "Microsoft Edge";
         browserOk = true;
     }
     // Detect Opera
     else if (/Opera/.test(userAgent)) {
-        browser = "Opera (Chromium)";
+        browser = "Opera";
         browserOk = true;
     }
     // Detect Firefox
@@ -530,7 +530,7 @@ function browserType() {
     }
 
     if (!browserOk) {
-        window.alert("EX-WebThrottle is only known to work on Chromium based web browsers. (i.e. Chrome, Edge, Opera).\n\n Your browser is NOT one of these, so EX-WebThrottle will likely not work. You will not be able to select or interact with the USB port.")
+        window.alert("EX-WebThrottle is only known to work on Chromium based web browsers. (i.e. Chrome, Edge, Opera).\n\n Your browser '" + browser + "' is NOT one of these, so EX-WebThrottle will likely not work. You will not be able to select or interact with the USB port.")
     }
     return "Browser: " + browser;
 }
