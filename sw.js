c5928721f067321281dd385bd7a7db425b2cceb2
Peter Akers <akersp62@gmail.com>
2024-03-03 08:21:27 +1000

    attempt to reduce caching

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 var cacheName = 'WebThrottle-EX';
 var filesToCache = [
-  '/WebThrottle-EX/',
-  'index.html',
   'images/favicon.ico',
   'images/carbon_fibre.png',
   'images/cover.png',
@@ -20,25 +18,12 @@ var filesToCache = [
   'css/layout.css',
   'css/pwa.css',
   'css/roundslider.min.css',
-  'css/settings.css',
-  'css/throttle.css',
   'css/fonts/icomoon.eot',
   'css/fonts/icomoon.svg',
   'css/fonts/icomoon.ttf',
   'css/fonts/icomoon.woff',
   'css/themes/dark.css',
   'css/themes/metallic.css',
-  'js/addloco.js',
-  'js/commandController.js',
-  'js/emulator.js',
-  'js/exwebthrottle.js',
-  'js/fnMaster.js',
-  'js/jquery-3.2.1.min.js',
-  'js/jquery-ui.min.js',
-  'js/jquery.rotaryswitch.js',
-  'js/pwa.js',
-  'js/roundslider.min.js',
-  'js/storageController.js'
 ];
 
 self.addEventListener('install', function(e) {
