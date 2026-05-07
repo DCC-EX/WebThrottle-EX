d216c1c40afa599486bb7446ecc9b16095c207e2
Peter Akers <akersp62@gmail.com>
2026-05-08 08:33:28 +1000

    Update commandController.js

--- js/commandController.js
+++ js/commandController.js
@@ -254,7 +254,11 @@ function parseResponse(cmd) {  // some basic ones only
             } else if (cmdArray[1] == 'CONSIST') {
                 try {
                     consistAddr = parseInt(cmdArray[2]);
-                    displayLog("[i] Consist Address: " + consistAddr);
+                    if (consistAddr >= 0) {
+                        displayLog("[i] Consist Address: " + consistAddr);
+                    } else {
+                        displayLog("[i] Consist Address Read Failed!");
+                    }
                 } catch (e) {
                     console.log(getTimeStamp + '[ERROR] Unable to process read consist address response');
                 }
